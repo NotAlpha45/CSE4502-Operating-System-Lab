fd45763b041915113d95bc8588b2f12b0488d5ea
Maheen Hoque <75400940+NotAlpha45@users.noreply.github.com>
2022-11-22 20:49:30 +0600

    implemented queue and process id

--- Lab Final/practice.py
+++ Lab Final/practice.py
@@ -1,34 +1,60 @@
-from multiprocessing import Process, Pipe
-from time import sleep
+from multiprocessing import Process, JoinableQueue, current_process, parent_process
+from queue import Empty
 
 
-def worker(id, task):
-    # sleep(1)
-    print(f"I am process {id}, doing {task}")
+def worker(queue: JoinableQueue):
 
-    with open(task+".txt", "w") as fh:
-        fh.write(task)
+    try:
+        item = queue.get()
+        id = item.get('id')
+        task = item.get('task')
+
+        # The enqued data has been read
+        queue.task_done()
+
+        process_id = current_process().pid
+
+        print(f"I am process: {id}, process id: {process_id}, doing: {task}")
+        print(f"My parent: {parent_process().pid}")
+        print()
+
+        with open(task+".txt", "w") as fh:
+            fh.write(task)
+
+    except Empty:
+        print("The queue is empty")
+        pass
 
 
 if __name__ == "__main__":
 
-    print("Master process started")
+    print(f"Master process started, process id: {current_process().pid}")
 
     process_list: list[Process] = []
+
     action_dict = {
         1: "palindrome_checking",
         2: "password_strngth_checking",
         3: "prime_number_generation"
     }
 
+    communication_queue = JoinableQueue()
+
     for i in range(1, 4):
+
+        communication_queue.put({
+            "id": i,
+            "task": action_dict[i]
+        })
+
         process_list.append(
-            Process(target=worker, args=(i, action_dict[i]))
+            Process(target=worker, args=(communication_queue,))
         )
 
     for process in process_list:
         process.start()
 
+    # Wait for the processes to finish
     for process in process_list:
         process.join()
 
